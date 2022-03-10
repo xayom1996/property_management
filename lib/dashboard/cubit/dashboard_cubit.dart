4d4aebc7491db6acd4e20304a08156e7f15ddaa5
xayom1996 <xayom1996@gmail.com>
2022-03-10 21:23:45 +0300

    some changes

--- lib/dashboard/cubit/dashboard_cubit.dart
+++ lib/dashboard/cubit/dashboard_cubit.dart
@@ -16,7 +16,7 @@ class DashboardCubit extends Cubit<DashboardState> {
   List<Widget> pages = [
     const ListObjectsPage(),
     CharacteristicsPage(),
-    Container(),
+    ExploitationPage(),
     Container(),
     Container(),
     // ExploitationPage(),
